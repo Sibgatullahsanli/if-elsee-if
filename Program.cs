45eaa3b75ffff89c7ac15721121c3316723bf4f3
Sıbgatullah <sibgat.sanli@gmail.com>
2022-08-26 23:51:20 +0300

    ilk yükleme

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int time = DateTime.Now.Hour;
+
+        if(time>=6 && time<11 )
+            Console.WriteLine("Hayırlı sabahlar");
+        else if(time<= 18)
+             Console.WriteLine("Hayırlı günler");
+        else
+             Console.WriteLine("Hayırlı geceler");
+
+        string sonuc = time<=18 ? "Hayırlı günler" : "Hayırlı geceler";
+
+        sonuc = time>=6 && time<11 ? "Hayırlı sabahlar" : time<= 18 ? "Hayırlı günler" : "Hayırlı geceler" ;
+        Console.WriteLine(sonuc);                  
+
+
+    }
+}
